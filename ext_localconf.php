908af5b22520236ec3eb380ceb46ae6117a2d73f
Steffen Kroggel <kroggel@rkw.de>
2019-11-07 12:25:52 +0100

    [REFACTORING] user_func for ext_localconf.php and ext_tables.php / fix for flashMessages

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,24 +1,36 @@
 <?php
-if (!defined('TYPO3_MODE')) {
-	die('Access denied.');
-}
+defined('TYPO3_MODE') || die('Access denied.');
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-	'RKW.' . $_EXTKEY,
-	'Infolayer',
-	array(
-		'Infolayer' => 'show, content, cookie, dismiss',
-		
-	),
-	// non-cacheable actions
-	array(
-		'Infolayer' => 'content, cookie, dismiss',
-		
-	)
+call_user_func(
+    function($extKey)
+    {
+
+        //=================================================================
+        // Configure Plugin
+        //=================================================================
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+            'RKW.' . $extKey,
+            'Infolayer',
+            array(
+                'Infolayer' => 'show, content, cookie, dismiss',
+
+            ),
+            // non-cacheable actions
+            array(
+                'Infolayer' => 'content, cookie, dismiss',
+
+            )
+        );
+
+        //=================================================================
+        // Add tables
+        //=================================================================
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages(
+            'tx_rkwinfolayer_domain_model_infolayer'
+        );
+    },
+    $_EXTKEY
 );
 
-//=================================================================
-// Add tables
-//=================================================================
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_rkwinfolayer_domain_model_infolayer', 'EXT:rkw_info_layer/Resources/Private/Language/locallang_csh_tx_rkwinfolayer_domain_model_infolayer.xlf');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_rkwinfolayer_domain_model_infolayer');
+
+
